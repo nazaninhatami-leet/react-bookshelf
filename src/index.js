3fd6d9b7a7509679722f4a609be19beabd8e8a74
nazanin hatami <nazaninhatami.leet@gmail.com>
2025-06-25 14:53:13 +0330

    added some inline style in jsx just to practice

--- src/index.js
+++ src/index.js
@@ -23,12 +23,15 @@ const BookImg = () => {
   return (
     <img
       src="https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg"
-      alt=""
+      alt="Book Image"
     />
   );
 };
 const BookName = () => <h1>The Let Them Theory: A Life-Changing Tool</h1>;
-const BookAuthor = () => <h2>Mel Robbins</h2>;
+const BookAuthor = () => {
+  const bookAuthor = { color: '#8d3592', letterSpacing: '0.1rem' };
+  return <h2 style={bookAuthor}>Mel Robbins</h2>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

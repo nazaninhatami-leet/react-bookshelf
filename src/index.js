d4d7cefb16c2e1c64c4fbc957c5c6981f0b57c5d
nazanin hatami <nazaninhatami.leet@gmail.com>
2025-06-25 15:06:34 +0330

    made book in one component and made the page more responsive with css media query

--- src/index.js
+++ src/index.js
@@ -3,39 +3,27 @@ import './index.css';
 
 const BookList = () => (
   <section className="booklist">
-    <Book />
-    <Book />
-    <Book />
+    <Book
+      bookName="The Let Them Theory: A Life-Changing Tool"
+      bookAuthor="Mel Robbins"
+    />
+    <Book bookName="Atomic Habits" bookAuthor="James Clear" />
   </section>
 );
 
-function Book() {
+function Book(props) {
   return (
     <div className="book">
-      <BookImg />
-      <BookName />
-      <BookAuthor />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg"
+        // src="logo192.png"
+        alt="Book Image"
+      />
+      <h1>{props.bookName}</h1>
+      <h2>{props.bookAuthor}</h2>
     </div>
   );
 }
-
-const BookImg = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg"
-      alt="Book Image"
-    />
-  );
-};
-
-const bookNameValue = 'The Let Them Theory: A Life-Changing Tool';
-const BookName = () => <h1>{bookNameValue}</h1>;
-
-const BookAuthor = () => {
-  const bookAuthorValue = 'Mel Robbins';
-  const bookAuthorStyles = { color: '#8d3592', letterSpacing: '0.1rem' };
-  return <h2 style={bookAuthorStyles}>{bookAuthorValue}</h2>;
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

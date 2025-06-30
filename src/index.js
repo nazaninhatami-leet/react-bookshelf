6cc7b3032db9d3e486e75db5bd4ddfc75886bd87
nazanin hatami <nazaninhatami.leet@gmail.com>
2025-06-30 14:38:34 +0330

    handled the Each child in a list should have a unique key prop error and the booklist height

--- src/index.js
+++ src/index.js
@@ -6,29 +6,34 @@ const books = [
     title: 'The Let Them Theory',
     author: 'Mel Robbins',
     img: 'images/HYYHStation.jpg',
+    id: 1,
   },
   {
     title: 'Atomic Habits',
     author: 'James Clear',
     img: 'images/suckerIsrael.jpg',
+    id: 2,
   },
   {
     title: 'Magic Shop',
     author: 'Some Human',
     img: 'images/goIran.jpg',
+    id: 3,
   },
   {
     title: 'Alice in Wonderland',
     author: 'Some Human',
     img: 'logo512.png',
+    id: 4,
   },
 ];
 
 const BookList = () => (
   <section className="booklist">
     {books.map((book) => {
-      const { title, author, img } = book;
-      return <Book title={title} author={author} img={img} />;
+      const { title, author, img, id } = book;
+      console.log(book);
+      return <Book title={title} author={author} img={img} key={id} />;
     })}
   </section>
 );
